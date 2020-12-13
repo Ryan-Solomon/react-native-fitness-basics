9c9c4287d9da221ec14c9d076f8b074a58c851c2
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-12-13 16:32:58 -0600

    update background style

--- src/styles/backgrounds.tsx
+++ src/styles/backgrounds.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 
 export const StyledBackgroundContainer = styled.View`
-  background-image: linear-gradient(#777777, #212121);
+  background-image: linear-gradient(to right, #222, #2d2d2d, #222);
   height: 100%;
 `;
